0feea2c7d123dee216891248e1c4369db3f7a541
Wei Shan <wshan@unc.edu>
2022-01-19 18:36:05 -0500

    beforeQuiz1

---
+++ lessons/RQ03.py
@@ -0,0 +1,12 @@
+choice: int = int(input("Enter a number: "))
+
+if choice < 50:
+    if choice < 25:
+        print("A")
+    else:
+        print("B")
+else:
+    if choice < 75:
+        print("D")
+    else:
+        print("C")
