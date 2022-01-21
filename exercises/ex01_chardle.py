39885940edaf2bad3ce956f78b99600c6e7a229e
Wei Shan <wshan@unc.edu>
2022-01-21 18:56:51 -0500

    exerciese01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -18,21 +18,24 @@ print("Searching for e in hello")
 char_count: int = 0
 
 if input_word[0] == input_char:
-    print(input_char + "found at index 0")
+    print(input_char + " found at index 0")
     char_count += 1
 if input_word[1] == input_char:
-    print(input_char + "found at index 1")
+    print(input_char + " found at index 1")
     char_count += 1
 if input_word[2] == input_char:
-    print(input_char + "found at index 2")
+    print(input_char + " found at index 2")
     char_count += 1
 if input_word[3] == input_char:
-    print(input_char + "found at index 3")
+    print(input_char + " found at index 3")
     char_count += 1
 if input_word[4] == input_char:
-    print(input_char + "found at index 4")
+    print(input_char + " found at index 4")
     char_count += 1
 if char_count > 0:
-    print(str(char_count) + " instance of " + input_char + " found in " + input_word)
+    if char_count == 1:
+        print(str(char_count) + " instance of " + input_char + " found in " + input_word)
+    else:
+        print(str(char_count) + " instances of " + input_char + " found in " + input_word)
 else:
     print("No instances of " + input_char + " found in " + input_word)
