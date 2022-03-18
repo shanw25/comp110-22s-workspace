e5960d8e96d529946e5fb289b2ce9b0212175a07
Wei Shan <wshan@unc.edu>
2022-03-18 17:28:51 -0400

    new changes

--- lessons/notebook_helpers.py
+++ lessons/notebook_helpers.py
@@ -4,3 +4,10 @@
 def add_two_ints(x: int, y: int) -> int:
     """Add x to y."""
     return x + y
+
+
+
+dd: dict[str, int] = {"a": 1, "b": 2}
+print(dd)
+dd.pop(1)
+print(dd)
--- lessons/test.py
+++ lessons/test.py
@@ -1,24 +1,16 @@
-def luhn(number: int) -> bool:
-    str_number: str = str(number)
-    result_number: list[int] = []
-    i: int = len(str_number) - 2
-    su: int = 0
-    result_number.append(int(str_number[i + 1]))
-    while i >= 0:
-        result_number.append(int(str_number[i]) * 2)
-        if (i - 1) >= 0:
-            result_number.append(int(str_number[i - 1]))
-        i -= 2
-    ind: int = 0
-    for index in result_number:
-        if len(str(index)) != 1:
-            result_number[ind] = result_number[ind] - 9
-            su += result_number[ind]
-            ind += 1
-        else:
-            su += result_number[ind]
-            ind += 1
-    # 这样就把它们同时也都加起来了，sum是在su这个变量里面
-    if su % 10 == 0:
-        return True
-    return False
+def locate_substring(dna_snippet, dna):
+    result = []
+    current_dna: str = ""
+    i: int = 0
+    while i < len(dna) - 4:
+        current_dna = dna[i]
+        current_dna += dna[i + 1]
+        current_dna += dna[i + 2]
+        current_dna += dna[i + 3]
+        if current_dna == dna_snippet:
+            result.append(i)
+        i += 1
+    return result
+
+
+print(locate_substring("ATAT", "GATATATGCATATACTT"))
